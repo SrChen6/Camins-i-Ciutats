3cf74c41267ac00215d2f6303d86191bba2bc89b
Haokang <haokang.chen@estudiantat.upc.edu>
2024-03-08 19:13:49 +0100

    get_board in game.py works

--- game.py
+++ game.py
@@ -16,21 +16,21 @@ class Game:
 
     def __init__(self): 
         """Constructor of the Game class"""
-        self._num_turns = yogi.read(int)
-        self._path_price = yogi.read(int)
-        self._city_price = yogi.read(int)
-        self._destr_price = yogi.read(int)
-        self._cash_o = yogi.read(int)
-        self._max_city = yogi.read(int)
-        self._board = get_board()
-
+        # self._num_turns = yogi.read(int)
+        # self._path_price = yogi.read(int)
+        # self._city_price = yogi.read(int)
+        # self._destr_price = yogi.read(int)
+        # self._cash_o = yogi.read(int)
+        # self._max_city = yogi.read(int)
+        self._board = self.get_board()
 
 
     def get_board(self) -> Board:
         """Returns the information of the board (size and resources)"""
-
-        size = board.get_size(yogi.read(int), yogi.read(int))
-        resources = [[read(int) for _ in range(size[1])]for _ in range(size[0])]
+        size = [yogi.read(int) for _ in range(2)]
+        resources = [[yogi.read(int) for _ in range(size[1])]for _ in range(size[0])]
+        print(size)
+        print(resources)
         return Board(size, resources)
 
         
@@ -38,8 +38,8 @@ class Game:
     def get_players(self) -> list[Player]:
         """Returns the number of players and their colors"""
 
-        self._num_players = read(int)
-        return [get_current_player() for _ in range(_num_players)]
+        self._num_players = yogi.read(int)
+        return [self.get_current_player() for _ in range(self._num_players)]
 
 
     def get_current_player(self) -> Player:
