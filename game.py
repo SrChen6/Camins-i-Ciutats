e7ff43a125c48e0a8164e7679cce10f13266c142
SrChen6 <haokangchen6@gmail.com>
2024-03-11 23:00:54 +0100

    elaborated next_turn from game.py, small changes to other things

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import places
 from player import Player
 from board import Board
-import yogi
+from yogi import read
 
 
 class Game:
@@ -19,37 +19,37 @@ class Game:
 
     def __init__(self): 
         """Constructor of the Game class"""
-        if yogi.read(str) == "number_turns": # algernativa: asser
-            self._num_turns = yogi.read(int)
-        if yogi.read(str) == "path_price":
-            self._path_price = yogi.read(int)
-        if yogi.read(str) == "city_price":
-            self._city_price = yogi.read(int)
-        if yogi.read(str) == "destruction_price":
-            self._destr_price = yogi.read(int)
-        if yogi.read(str) == "initial_cash":
-            cash_o = yogi.read(int)
-        if yogi.read(str) == "max_cities":
-            self._max_city = yogi.read(int)
-        if yogi.read(str) == "board_size":
-            size = [yogi.read(int) for _ in range(2)]
-            resources = [[yogi.read(int) for _ in range(size[1])]for _ in range(size[0])]
+        if read(str) == "number_turns": # algernativa: asser
+            self._num_turns = read(int)
+        if read(str) == "path_price":
+            self._path_price = read(int)
+        if read(str) == "city_price":
+            self._city_price = read(int)
+        if read(str) == "destruction_price":
+            self._destr_price = read(int)
+        if read(str) == "initial_cash":
+            cash_o = read(int)
+        if read(str) == "max_cities":
+            self._max_city = read(int)
+        if read(str) == "board_size":
+            size = [read(int) for _ in range(2)]
+            resources = [[read(int) for _ in range(size[1])]for _ in range(size[0])]
             self._board = Board(size, resources, [], [])
-            print(self._board._resources)
-        if yogi.read(str) == "num_players":
-            self._num_players = yogi.read(int)
+        if read(str) == "num_players":
+            self._num_players = read(int)
         self._players = []
         for n in range(self._num_players): # color
-            if yogi.read(str) == "player_color":
-                self._players.append(Player(n, cash_o, yogi.read(str)))
+            if read(str) == "player_color":
+                self._players.append(Player(n, cash_o, read(str)))
         self._players = self.get_players()
         for player in self._players: #first city
-            if yogi.read(str) == "player_city":
-                self._board._citites.append([player, [yogi.read(int),yogi.read(int)]])
+            if read(str) == "player_city":
+                self._board._citites.append([player, [read(int),read(int)]])
+
+        self._current_turn = 0
 
 
-    def get_board(self) -> Board: # No és la informació inicial, símplement retorna la info
-        # tota la lectura inicial es fa dins de init
+    def get_board(self) -> Board:
         """Returns the information of the board (size and resources)"""
         return self._board
 
@@ -61,7 +61,6 @@ class Game:
 
     def get_current_player(self) -> Player:
         """Returns the id, the cash and the color of a single player"""
-        #Not sure what it does, input is all colors followed by all cities
         return self._players[self._current_turn%self._num_players + 1]
 
 
@@ -72,4 +71,19 @@ class Game:
 
     def next_turn(self) -> None:
         """takes input of the next turn"""
-        ...
+        self._current_turn += 1
+        action = read(str)
+        player = self.get_current_player()
+        if read(int) == player._id:
+            match action: 
+                case "build_path":# don't forget the changes to cash
+                    coord1 = tuple[read(int), read(int)]
+                    coord2 = tuple[read(int), read(int)]
+                    self._board.add_path(player, tuple[coord1, coord2])
+                case "build_city":
+                    self._board.add_city(player, tuple[read(int), read(int)])
+                case "destroy_city":
+                    self._board.remove_city(tuple[read(int), read(int)])
+        else:
+            print("Invalid action, turn cancelled")
+
